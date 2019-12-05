f1b653a1583ebb04702839ba37b623392a4a24e7
Gin <1223137796@qq.com>
2019-12-05 18:10:21 +0800

    购课车增加删除

--- js/cart.js
+++ js/cart.js
@@ -1,9 +1,49 @@
-window.onload = function () { 
-	var sourceNode = document.getElementById("content-0"); // 获得被克隆的节点对象 
-	for (var i = 1; i < 6; i++) { 
-		var clonedNode = sourceNode.cloneNode(true); // 克隆节点 
-		clonedNode.setAttribute("id", "content-" + i); // 修改一下id 值，避免id 重复 
-		sourceNode.parentNode.appendChild(clonedNode); // 在父节点插入克隆的节点 
+window.onload = function () {
+	var data = [
+		{ img: 'image/bg-0.png', 
+		title: '士大夫士大夫随风倒十分的供货合同今天要凤飞飞法山豆根豆腐干豆腐干的武打风格个多个地方换个地方', 
+		hour: '15', url: '1', name: '网络教育', price: '1564', coin: '22'},
+		{ img: 'image/bg-0.png', 
+		title: '克里夫获利丰厚瑞龙发v那给发给对方就会改恶个发果断减肥古额复读了个发uiu啊了官府关啦房贷首付', 
+		hour: '12', url: '2', name: '人民教育', price: '1467', coin: '46'},
+		{ img: 'image/bg-0.png', 
+		title: '更符合健康全额退几个好菜干部选拔v想日法国try让他一人图如今的刚好四个人的风格个十分广泛大概', 
+		hour: '16', url: '3', name: '安徽教育', price: '1545', coin: '24'},
+		{ img: 'image/bg-0.png', 
+		title: '发顺丰到付的公共电话亭教育集团电风扇广泛故事感人太热也页也分公司管饭官方给他然后try人', 
+		hour: '13', url: '4', name: '中国教育', price: '1267', coin: '39'},
+		{ img: 'image/bg-0.png', 
+		title: '啊发娥夫人try徒弟师傅v梵蒂冈地方很多个放到公司官方的手感温润特意让他一人兔兔上市公司股份地方', 
+		hour: '18', url: '5', name: '科技教育', price: '1366', coin: '62'},
+	]
+	var content = document.getElementById("content-0"); // 获得被克隆的节点对象 
+	for (var i = 1, k = 0; i < 6; i++, k++) { 
+		var clonedNode = content.cloneNode(true); // 克隆节点 
+		var oItems = clonedNode.childNodes;
+		clonedNode.setAttribute("id", "content-" + i); // 修改一下id 值，避免id 重复
+		for (var j = 1; j < 13;) {
+			oItems[j].setAttribute("id", oItems[j].className+ "-" + i);
+			// console.log(oItems[j].id);
+			j += 2;
+		}
+		var cn1 = oItems[5].childNodes[1];
+		var cn2 = oItems[7].childNodes[3];
+		var cn3 = oItems[7].childNodes[7];
+		var cn4 = oItems[5].childNodes[5];
+		var cn5 = oItems[5].childNodes[3].childNodes[3];
+		cn1.setAttribute("id", cn1.className+ "-" + i);
+		cn2.setAttribute("id", cn2.className+ "-" + i);
+		cn3.setAttribute("id", cn3.className+ "-" + i);
+		cn5.setAttribute("id", cn5.className+ "-" + i);
+		cn4.setAttribute("id", cn4.className+ "-" + i);
+		cn1.innerHTML = data[k].title;
+		cn5.innerHTML = data[k].hour;
+		cn2.innerHTML = data[k].price;
+		cn3.innerHTML = data[k].coin;
+		oItems[3].setAttribute("src", data[k].img);
+		cn4.setAttribute("href", data[k].url);
+		cn4.innerHTML = data[k].name;
+		content.parentNode.appendChild(clonedNode); // 在父节点插入克隆的节点 
 	} 
 };
 
@@ -19,20 +59,63 @@ var wh = $(window).height();
     $(".main").css({width: mw +'px',height: mh +'px',});
 })(document, window);
 
-$(".choose").click(function(event) {
-	var c = $(".choose_img");
+function choose(e) {
+	var c = $('#'+e).children('img:first');
+	$("#choose_all").css({border:'1px solid rgba(187,187,187,1)',});
+	$("#choose_all").children('img:first').css({display: 'none',});
 	if(c.css("display") == "block") {
 		c.css({
 			display: 'none',
 		});
-		$(this).css({
+		$('#'+e).css({
 			border:'1px solid rgba(187,187,187,1)',
 		});
 	}else {
 		c.css({
 			display: 'block',
 		});
-		$(this).css({
+		$('#'+e).css({
+			border:'0px',
+		});
+	}
+};
+
+function pay_choose(e) {
+	var c = $('#'+e).children('img:first');
+	if(c.css("display") == "block") {
+		c.css({
+			display: 'none',
+		});
+		$('#'+e).css({
+			border:'1px solid rgba(187,187,187,1)',
+		});
+	}else {
+		$(".pay_choose_img").css({display: 'none',});
+		$(".pay_choose").css({border:'1px solid rgba(187,187,187,1)',});
+		c.css({
+			display: 'block',
+		});
+		$('#'+e).css({
+			border:'0px',
+		});
+	}
+}
+
+$("#choose_all").click(function(event) {
+	var ca = $("#choose_all").children('img:first');
+	var c = $(".choose_img");
+	if(ca.css("display") == "block") {
+		c.css({
+			display: 'none',
+		});
+		$(".choose").css({
+			border:'1px solid rgba(187,187,187,1)',
+		});
+	}else {
+		c.css({
+			display: 'block',
+		});
+		$(".choose").css({
 			border:'0px',
 		});
 	}
@@ -44,19 +127,19 @@ $(".add").click(function() {
   
 $(".sub").click(function() {
 	var i = $(this).next().val();
-	if(i != "0"){
+	if(i != "1"){
     	$(this).next().val(parseInt($(this).next().val()) - 1);
 	}else {
 		$(this).attr('readonly','true');
 	}
 });
 
-$(".content").on("touchstart", function(e) {
+$('.content').on("touchstart", function(e) {
     startX = e.originalEvent.changedTouches[0].pageX,
     startY = e.originalEvent.changedTouches[0].pageY;
 });
 
-$(".content").on("touchmove", function(e) {
+$('.content').on("touchmove", function(e) {
     moveEndX = e.originalEvent.changedTouches[0].pageX,
     moveEndY = e.originalEvent.changedTouches[0].pageY,
     X = moveEndX - startX,
@@ -64,14 +147,14 @@ $(".content").on("touchmove", function(e) {
       
     if ( Math.abs(X) > Math.abs(Y) && X > 0 ) {
     	$(this).removeClass('content_left');
-    	$(".delete").removeClass('delete_right');
+    	$(this).children('div:last').removeClass('delete_right');
 		setTimeout(function(){
     		$(".delete_p").css({display: 'none',});
     	},100);
     }
     else if ( Math.abs(X) > Math.abs(Y) && X < 0 ) {
     	$(this).addClass('content_left');
-    	$(".delete").addClass('delete_right');
+    	$(this).children('div:last').addClass('delete_right');
     	setTimeout(function(){
     		$(".delete_p").css({display: 'block',});
     	},200);
@@ -101,4 +184,10 @@ function check() {
 $(".curtain").click(function() {
 	$(this).css({display: 'none',});
 	$(".pay").removeClass('pay_top');
+});
+
+$(".delete").click(function(event) {
+	$(this).parent().css({'transform':'translateX(-125%)', 'height':'0px', 'margin':'0px',});
+	$(this).parent().delay(1000).hide(0);
+	console.log('测试');
 });
